9afd23bd0450f60ceecdbc2d3667601ae6040150
Jonas Stalder <jonas.stalder@exanic.ch>
2024-11-14 16:59:37 +0100

    finaler stand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+export class Fussballer{
+  constructor(
+    public Name:string,
+    public Alter:number,
+    public Goals:number,
+    public Bild :string,
+  ){}
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +18,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Matteo';
+  title = 'Matteo ⚽';
+  Fussballerliste = [
+    new Fussballer("Lionel Messi",37,850,"messi.webp"),
+    new Fussballer ("Lamine Yamal",17,13,"lamineyamal.webp"),
+    new Fussballer ("Cristiano Ronaldo",39,908,"Ronaldo.webp"),
+  ];
+
+  GewaehlterFussballer:Fussballer=this.Fussballerliste[0];
+  InfosAnzeigen = false;
+
+  onChange(event: any){
+    const auswahl=event.target.value;
+    console.log(auswahl);
+
+    if(auswahl=="Bitte wählen"){
+      this.InfosAnzeigen=false;
+    }
+    else{
+      this.InfosAnzeigen=true;
+      const index=this.Fussballerliste.findIndex(x=>x.Name==auswahl);
+      this.GewaehlterFussballer=this.Fussballerliste[index];
+    }
+  }
 }
